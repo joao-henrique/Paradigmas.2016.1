587e05a4cf98c28f3803c401fdab7dda42b2db3d
joao-henrique <joaohenrique.pereira.almeida@gmail.com>
2016-05-06 11:50:57 -0300

    Prolog add

---
+++ Prolog/project.pl
@@ -0,0 +1,45 @@
+city(Brasilia).
+city(São Paulo).
+city(Fortaleza).
+city(Brasilia).
+city(Manaus).
+city(Santa Catarina).
+city(Rio de Janeiro).
+city(Rio Grande doSul).
+
+estacao(verao):-
+  mes(dezembro),
+  mes(janeiro),
+  mes(fevereiro).
+estacao(inverno):-
+  mes(junho),
+  mes(julho),
+  mes(agosto).
+estacao(outono):-
+  mes(marco),
+  mes(abril),
+  mes(maio).
+estacao(primavera):-
+  mes(setembro),
+  mes(outubro),
+  mes(novembro).
+
+
+
+
+lazer(praia,Santa Catarina).
+lazer(praia,Fortaleza).
+lazer(historico,Santa Catarina).
+lazer(natureza,Santa Catarina).
+
+clima(inverno,Serra Gaúcha)
+clima(inverno,Gramado)
+clima(inverno,Cambará doSul)
+clima(inverno, São Joaquin, Santa Catarina)
+clima(frio,Santa Catarina).
+clima(calor,Fortaleza).
+clima(calor,Rio de Janeiro).
+
+
+
+viajem(Destino)
